7835c409e86324c073c0b06ea86d9ba8ea53fb97
Carl Reinecken <carl.reinecken@syracom.de>
2018-09-01 10:58:03 +0200

    fixing database variable name

--- User.php
+++ User.php
@@ -14,26 +14,26 @@ class User extends KiwiMeta {
 
     public function friend()
     {
-        return (new $this($this->_db))
+        return (new $this($this->database))
             ->find($this->friend_id);
     }
 
     public function creator()
     {
-        return (new $this($this->_db))
+        return (new $this($this->database))
             ->find($this->created_by);
     }
 
     public function user_created()
     {
-        return (new $this($this->_db))
+        return (new $this($this->database))
             ->where('created_by = ', $this->id)
             ->all();
     }
 
     public function updater()
     {
-        return (new $this($this->_db))
+        return (new $this($this->database))
             ->find($this->updated_by);
     }
 
